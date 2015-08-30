5537fbdd7b79c40be04024f871f3fccf33d08e45
Stefan Hedman <schteppe@gmail.com>
2015-08-30 12:17:55 +0200

    initial commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+This archive contains a template for hosting your Goo Create project on a web server.
+Look in index.html for some guidance on how to do this.
---
+++ js/shims.js
@@ -0,0 +1,29 @@
+// Modified version of https://gist.github.com/devongovett/1381839 (removed unused stuff)
+if (!("classList" in document.documentElement) && Object.defineProperty && typeof HTMLElement !== 'undefined') {
+    Object.defineProperty(HTMLElement.prototype, 'classList', {
+        get: function() {
+            var self = this;
+            function update(fn) {
+                return function(value) {
+                    var classes = self.className.split(/\s+/),
+                        index = classes.indexOf(value);
+
+                    fn(classes, index, value);
+                    self.className = classes.join(" ");
+                }
+            }
+
+            var ret = {
+                add: update(function(classes, index, value) {
+                    ~index || classes.push(value);
+                }),
+
+                remove: update(function(classes, index) {
+                    ~index && classes.splice(index, 1);
+                })
+            };
+
+            return ret;
+        }
+    });
+}
